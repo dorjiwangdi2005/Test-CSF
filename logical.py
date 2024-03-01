51b37d2f1977c136b27041f85d2452782160d64f
dorjiwangdi2005 <02230063.cst@rub.edu.bt>
2024-03-01 15:09:38 +0600

    using logical operator

---
+++ logical.py
@@ -0,0 +1,17 @@
+# logical oprerator and
+x = 9
+print (x>3 and x<10)
+
+print (x > 3 and x  == 0)
+
+# logical oprerator or
+ y = 5
+ print (y<3 or y>10)
+
+ z = 12 
+ print (z > 10 or z % 2 == 0)
+
+ # logical oprerator not 
+print (not( x>3 and x<10))
+
+
