fc4a3fb806e63db466aadf8de982451c0fd10e65
dorjiwangdi2005 <02230063.cst@rub.edu.bt>
2024-03-01 15:34:30 +0600

    basic arithmetic operation

---
+++ opertation.py
@@ -0,0 +1,9 @@
+name = input ("enter your name: ")
+print ("wellcome ", name ," to our basic arithmetic operation")
+
+num_1 = float(input("enter a first number:"))
+num_2 = float(input("enter a second number:"))
+
+addition_result = num_1 + num_2
+print (addition_result)
+
