159d3142746f0bc379f1f195b5f1aebdd394c3be
dorjiwangdi2005 <02230063.cst@rub.edu.bt>
2024-05-18 13:59:43 +0600

    OOP methods

---
+++ P_OOP.py
@@ -0,0 +1,18 @@
+class dog:
+    def __init__ (self, name, color, age):
+        self.name = name
+        self.color = color
+        self.age = age
+
+obj_1 = dog ("dawa:", "white:" , 4)
+obj_2 = dog ("nima:", "brown:" , 2)
+obj_3 = dog ("black:", "black:" , 5)
+
+print (obj_1.name, obj_1.color, obj_1.age)
+print (obj_2.name, obj_2.color, obj_2.age)
+print (obj_3.name, obj_3.color, obj_3.age)
+
+
+
+            
+
