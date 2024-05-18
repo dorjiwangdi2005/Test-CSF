beba7ceaf972aa0df2ae1aa651fa007a3c1194af
dorjiwangdi2005 <02230063.cst@rub.edu.bt>
2024-05-18 18:00:36 +0600

    reverse the string

--- reverse_string.py
+++ reverse_string.py
@@ -6,6 +6,6 @@ def reverse_string(s):
     else:
     # Recursive case: reverse the substring excluding the last character
         return s[-1] + reverse_string(s[:-1])
+        
 a = str(input ("enter any word to be reversed: "))
 print(reverse_string(a))
-    
