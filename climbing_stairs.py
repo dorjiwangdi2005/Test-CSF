1ebf28034c027608bde08c1822f95f886a02f479
dorjiwangdi2005 <02230063.cst@rub.edu.bt>
2024-05-28 16:26:43 +0600

    climbing stairs using recursion

---
+++ climbing_stairs.py
@@ -0,0 +1,17 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        return self.ways(n,memo)
+
+    def ways(self,n,memo):
+        if n in memo:
+            return memo[n]
+
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+
+        memo[n] = self.ways(n-1,memo) + self.ways(n-2, memo)
+        return memo[n]
+        
